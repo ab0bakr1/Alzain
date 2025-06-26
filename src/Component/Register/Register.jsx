f56060f707de361351d0a806a96248c5ff8e5280
Abobakr Almashhor <121812837+ab0bakr1@users.noreply.github.com>
2025-06-27 00:41:52 +0300

    Update Register.jsx

--- src/Component/Register/Register.jsx
+++ src/Component/Register/Register.jsx
@@ -21,7 +21,7 @@ const Register = () => {
     }
 
     try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/auth/register`, {
+      const response = await fetch('https://alzain-production.up.railway.app/api/auth/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
